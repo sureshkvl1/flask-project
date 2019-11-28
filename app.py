915c00cebc70ea99ef2d1d14293e7efa6e5bc798
suresh <sureshkumarr.s@gmail.com>
2019-11-28 17:27:29 +0530

    u

--- app.py
+++ app.py
@@ -6,6 +6,6 @@ app = Flask(__name__)
 @app.route('/')
 def hello_whale():
     #return render_template("whale_hello.html")
-    return '<html><body bgcolor="#FFE00F"><h1>hello world</h1></body></html>'
+    return '<html><body bgcolor="#E6E00F"><h1>hello world</h1></body></html>'
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
