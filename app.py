a231c4eaba94996cc486a7d24bdd0db4c02d9f1d
suresh <sureshkumarr.s@gmail.com>
2019-12-04 15:54:28 +0530

    updated

--- app.py
+++ app.py
@@ -6,6 +6,6 @@ app = Flask(__name__)
 @app.route('/')
 def hello_whale():
     #return render_template("whale_hello.html")
-    return '<html><body bgcolor="#E6E000"><h1>hello Suresh Kumar</h1></body></html>'
+    return '<html><body bgcolor="#E6E000"><h1>hello Suresh Kumar1</h1></body></html>'
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
